21df9a1e6f47065790d4c3699ac1f8a7cdeaefab
Martin Grünbaum <martin@itsolveonline.net>
2026-06-08 18:55:55 +0200

    Fix animations on model loading

--- godot/projects/client/src/entities/model_manager.gd
+++ godot/projects/client/src/entities/model_manager.gd
@@ -3,6 +3,8 @@ extends Node3D
 
 signal base_model_set(container_node: Node3D, scene: PackedScene)
 
+@export var entity_root_node: BaseEntity
+
 var _model: Node3D
 
 func _ready():
@@ -15,6 +17,9 @@ func load_base_model(scene: PackedScene):
 
 	_model = scene.instantiate()
 	self.add_child(_model)
+	var animTree: ModelAnimationTree = _model.get_node_or_null("%AnimationTree")
+	if animTree:
+		animTree.bind_expression_base(entity_root_node)
 	base_model_set.emit(self, scene)
 	pass
 
